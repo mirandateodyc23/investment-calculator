b543dad592a3fc85468fcad2a68c5062ae3698cd
Teody Miranda <tmiranda@pacificseainvests.com>
2024-08-16 13:45:45 +0800

    migrate to ngmodules

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,9 @@
 import { Component, computed, inject} from '@angular/core';
 
-import { CommonModule } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
